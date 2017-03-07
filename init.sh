260318f8e5af29a5938ada1d72ae9308895bd065
Eric Heaton <eric.heaton@b-reel.com>
2017-03-07 17:49:39 -0500

    First iteration of an init script
    ** UNTESTED**
    
    Additional work to be done on it.

--- init.sh
+++ init.sh
@@ -1,1 +1,25 @@
-ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
+#!/bin/bash
+
+# Install oh-my-zsh
+if [[ -n $( echo $SHELL | grep "zsh" ) ]]; then
+    if [[ ! -d "~/.oh-my-zsh" ]]; then
+        sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+    else
+        echo "oh-my-zsh Alredy installed!"
+    fi
+else
+    echo "ZShell not set up. Please make default shell: "
+    echo "\t\$ chsh /bin/zsh"
+fi
+
+# Install BRP
+ln -s ~/.dotfiles/brp/.brprc ~/
+
+# Set up .zshrc
+ln -s ~/.dotfiles/zsh/.zshrc ~/
+
+# Set up .nanorc config
+ln -s ~/.dotfiles/nano/.nanorc ~/
+
+# Set up Multitial
+ln -s ~/.dotfiles/multitail/.multitailrc ~/
