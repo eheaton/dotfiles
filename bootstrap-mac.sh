983143d6545e7317676be6ef5e392634263b6ce8
Eric Heaton <eric.heaton@b-reel.com>
2019-03-22 16:58:47 -0400

    feat(bootstrap): Working on Apps that get installed (WIP)

--- bootstrap-mac.sh
+++ bootstrap-mac.sh
@@ -6,7 +6,6 @@ set -e
 [ `xcode-select -p 1>/dev/null; echo $?` ] || xcode-select --install
 
 # Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 [ `which brew` ] || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install Homebrew cask
@@ -15,6 +14,7 @@ brew tap caskroom/cask
 # Install Mac App store CLI
 [ `which mas` ] || brew install mas
 [ `which wget` ] || brew install wget
+
 echo "Please take a minute to sign into the Mac App Store under your account..."
 open -a "App Store"
 read -n 1 -s -r -p "(press any key to continue)"
@@ -26,96 +26,107 @@ read -n 1 -s -r -p "(press any key to continue)"
 # ======================================================
 
 # - 1Password
-    mas install 443987910
+  mas install 443987910
 # - Bear
-    mas install 1091189122
+  mas install 1091189122
 # - The Clock
-    mas install 488764545
+  mas install 488764545
 # - Converto
-    mas install 576421334
+  mas install 576421334
 # - Divvy
-    mas install 413857545
-# - Icon Slate
-    mas install 439697913
+  mas install 413857545
 # - iPhoto
-    mas install 408981381
+  mas install 408981381
 # - MediaInfo
-    mas install 510620098
+  mas install 510620098
 # - Reeder
-    mas install 880001334
-# - Slack
-    mas install 803453959
+  mas install 880001334
 # - Textual 5
-    mas install 896450579
+  mas install 896450579
 # - Tweetbot
-    mas install 557168941
+  mas install 557168941
 # - XCode
-    mas install 497799835
-
-# - Omnigraffle
-    mas install 1142578753
-
-# - Markoff
-    mas install 1084713122
-# - Dash
-    mas install 449589707
-# - Marked 2
-    mas install 890031187
-# - Better Rename 9
-    mas install 414209656
+  mas install 497799835
 
 # Run the upgrade to ensure we're on the latest of
 # all of the above
 mas upgrade
+
+exit 0
 
 # ======================================================
 # Apps to install via. Brew Cask
 # ======================================================
 
 # - Adobe Creative Cloud
-    brew cask install adobe-creative-cloud
+  brew cask install adobe-creative-cloud
 # - Charles
-    brew cask install charles
+  brew cask install charles
 # - CloudApp
-    brew cask install cloudapp
+  brew cask install cloudapp
 # - DiskMaker X?
-    brew cask install diskmaker-x
+  brew cask install diskmaker-x
 # - Kaelidoscope
-    brew cask install kaleidoscope
+  brew cask install kaleidoscope
 # - GitTower
-    brew cask install tower
+  brew cask install tower
 # - Paw
-    brew cask install paw
+  brew cask install paw
 # - phpStorm
-    brew cask install phpstorm
+  brew cask install phpstorm
 # - Sip
-    brew cask install sip
+  brew cask install sip
 # - Sublime Text
-    brew cask install sublime-text
+  brew cask install sublime-text
 # - Sequel Pro
-    brew cask install sequel-pro
+  brew cask install sequel-pro
 # - Quicksilver
-    brew cask install quicksilver
+  brew cask install quicksilver
 # - Insomnia
-    brew cask install insomnia
+  brew cask install insomnia
 # - Choosey
-    brew cask install choosey
+  brew cask install choosey
 # - VLC
-    brew cask install vlc
+  brew cask install vlc
 # - Spotify
-    brew cask install spotify
+  brew cask install spotify
 # - Microsoft Office
-    brew cask install Microsoft-office
+  brew cask install Microsoft-office
+# - Table Plus (database viewer)
+  brew cask install tableplus
+# - Postman (REST client Tool)
+  brew cask install postman
+# - Sketchup (3d Modeling)
+  brew cask install sketchup-pro
+# - Adobe Creative Cloud
+  brew cask install adobe-creative-cloud
+# - Gittower
+  brew cask install tower
+# - CloudApp
+  brew cask install cloudapp
+# - Sublime Text
+  brew cask install sublime-text
+# - Whatsapp Desktop Tool
+  brew cask install whatsapp
+# - Shift (mail client)
+  brew cask install shift
+# - Wacom Drivers
+  brew cask install homebrew/cask-drivers/wacom-intuos3-tablet
+# - Touch Designer
+  brew cask install touchdesigner
+# - iTerm
+  brew cask install iterm2
 
 # - Quicklook Plugins
-    brew cask install qlmarkdown
-    brew cask install qlcolorcode
-    brew cask install qlstephen
-    brew cask install quicklook-json
-    brew cask install quicklook-csv
-    brew cask install qlimagesize
-    brew cask install webpquicklook
-    brew cask install qlvideo
+  brew cask install qlmarkdown
+  brew cask install qlcolorcode
+  brew cask install qlstephen
+  brew cask install quicklook-json
+  brew cask install quicklook-csv
+  brew cask install qlimagesize
+  brew cask install webpquicklook
+  brew cask install qlvideo
+
 
 # ======================================================
 # Command Line Apps
@@ -137,6 +148,42 @@ mas upgrade
     brew install gifsicle
 
     brew install automake autoconf libtool libpng
+
+# - FFMPEG
+  # Tap the correct repo (that allows for flags)
+  brew tap varenc/ffmpeg
+  # Make this the default when installing FFMPEG
+  tap-pin varenc/ffmpeg
+  # Install FFMPEG with all deps / flags
+  brew install ffmpeg \
+    --with-chromaprint \
+    --with-fdk-aac \
+    --with-game-music-emu \
+    --with-libbluray \
+    --with-libbs2b \
+    --with-libcaca \
+    --with-libgsm \
+    --with-libmodplug \
+    --with-librsvg \
+    --with-libsoxr \
+    --with-libssh \
+    --with-libvidstab \
+    --with-libvmaf \
+    --with-opencore-amr \
+    --with-openh264 \
+    --with-openjpeg \
+    --with-openssl \
+    --with-rtmpdump \
+    --with-rubberband \
+    --with-speex \
+    --with-srt \
+    --with-tesseract \
+    --with-two-lame \
+    --with-wavpack \
+    --with-webp \
+    --with-xvid \
+    --with-zeromq \
+    --with-zimg
 
 # ======================================================
 # System Preferences
@@ -198,4 +245,7 @@ defaults write com.apple.LaunchServices LSQuarantine -bool false
 defaults write com.apple.dock "wvous-bl-corner" -int 5
 defaults write com.apple.dock "wvous-bl-modifier" -int 0
 
+# TODO:
+# Install SSH Keys
+# Setup preferences (iterm, quicksilver, etc...)
 # - Install SSH keys
